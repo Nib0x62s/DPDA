a31e7f542db98e261113035afae46d84405aa46b
Adrian <32743531+adrianvasu@users.noreply.github.com>
2019-04-16 21:58:18 -0400

    commit

--- DPDA.py
+++ DPDA.py
@@ -23,20 +23,33 @@ config = dict.fromkeys(['Q', 'Sigma', 'Gamma', 'Delta', 'S', 'I', 'F'], None)
 def dpda_simulator():
     my_input = input()
 
+    for x in my_input:
+        if (not my_input[x] in config['Sigma']):
+            print('Error')
+
 def setup_config(config_location):
     global config
 
     with open(f'{config_location}Q.conf') as f:
         config['Q'] = f.read(1)
 
-
-    # Still dosen't work from here down going out to eat :)
     with open(f'{config_location}Sigma.conf') as f:
         config['Sigma'] = f.read()
 
+    with open(f'{config_location}Gamma.conf') as f:
+        config['Gamma'] = f.read()
+
+    with open(f'{config_location}Delta.conf') as f:
+        config['Delta'] = f.read()
+
+    with open(f'{config_location}F.conf') as f:
+        config['F'] = f.read()
+
+    dpda_simulator()
+
 def main(argv):
     if len(sys.argv) < 2:
-        usage_string = f'usage: python {argv[0]} /location/of/configuration/files'
+        usage_string = f'usage: python {argv[0]} /location/of/configuration/files/'
         print(usage_string)
     else:
         setup_config(argv[1])
