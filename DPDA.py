e97f453dd914df2113789b4f91de1150d1c7d981
Skyler Knecht <nibbs@Skylers-MacBook-Pro.local>
2019-04-16 20:15:23 -0400

    Added Config files & Setup some configuration variables.

--- DPDA.py
+++ DPDA.py
@@ -2,23 +2,44 @@
     Python Version:     3.7.2
     Author:             Skyler Knecht
     Description:        Python script that simulates a deterministic pushdown automaton (DPDA).
-                        Accpeting input (e.g., aabbaa), the program will compute whether the provided
-                        input is Accepted or Rejected.
+                        Accpeting input (e.g., aabbaa) and speicific configuration files, the program
+                        will compute whether the provided input is Accepted or Rejected.
+
+    Variable Descriptions: (Q, Sigma, Gamma, Delta, S, I, F)
+            * Q         Largest state subscript.
+            * Sigma     A finite set of input symbols.
+            * Gamma     A finite stack alphabet.
+            * Delta     The transition functions.
+            * S         The start state.
+            * I         The initial contents of the stack.
+            * F         The final state.
 '''
 
 import sys
 
-def setup_config():
-    pass
+# Configuration Variables
+config = dict.fromkeys(['Q', 'Sigma', 'Gamma', 'Delta', 'S', 'I', 'F'], None)
 
+def dpda_simulator():
+    my_input = input()
+
+def setup_config(config_location):
+    global config
+
+    with open(f'{config_location}Q.conf') as f:
+        config['Q'] = f.read(1)
+
+
+    # Still dosen't work from here down going out to eat :)
+    with open(f'{config_location}Sigma.conf') as f:
+        config['Sigma'] = f.read()
 
 def main(argv):
     if len(sys.argv) < 2:
         usage_string = f'usage: python {argv[0]} /location/of/configuration/files'
         print(usage_string)
     else:
-        setup_config()
-
+        setup_config(argv[1])
 
 if __name__ == '__main__':
     main(sys.argv)
