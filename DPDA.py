0bbd9e3e0dd2f8928ac6b91b95ac74317ba7004f
Adrian <32743531+adrianvasu@users.noreply.github.com>
2019-04-17 00:37:44 -0400

    maybe works?

--- DPDA.py
+++ DPDA.py
@@ -19,6 +19,8 @@ import sys
 
 # Configuration Variables
 config = dict.fromkeys(['Q', 'Sigma', 'Gamma', 'Delta', 'S', 'I', 'F'], None)
+stackContents = None
+curState = None
 
 def dpda_simulator():
     my_input = input()
@@ -26,19 +28,47 @@ def dpda_simulator():
     global stackContents
     global curState
 
-    for x in my_input:
+    for x in range(0, len(my_input) - 1):
         if (not my_input[x] in config['Sigma']):
-            print('Error')
+            print('Error input is not in alphabet') # Invalid alphabet character
             break
         else:
             if (x == 0):
-                currentState = [my_input[x], my_input[x+1], config['I'], config['S']]
-                transition(currentState)
+                currentState = [my_input[x], config['I'], config['S']]
+                if (!transition(currentState)):
+                    print('Error no more transitions available')
+                    break
             else:
-                currentState = [my_input[x], my_input[x+1], stackContents, curState]
+                currentState = [my_input[x], stackContents, curState]
+                if (!transition(currentState)):
+                    print('Error no more transitions available')
+                    break
+        if (x == len(my_input) - 1):
+            print('String is accepted')
 
 def transition(currentState):
-    
+    cs = currentState[2]
+    cc = currentState[0]
+    ts = currentState[1][len(stackContents) - 1]
+    foundTransition = False
+
+    for x in range(0, len(config['Delta'])):
+        if (config['Delta'][x][2] == cc && config['Delta'][x][4] == ts && config['Delta'][0] == cs):
+            stackContents = currentState[1] + config['Delta'][x][8:]
+            curState = config['Delta'][x][6]
+            foundTransition = True
+            return True
+
+    if (!foundTransition):
+        for x in range(0, len(config['Delta'])):
+            if (config['Delta'][x][0] == 'L' && config['Delta'][x][2] == cc && config['Delta'][x][4] == ts):
+                stackContents = currentState[1] + config['Delta'][x][8:]
+                curState = config['Delta'][x][6]
+                return True
+            else:
+                return False
+
+
 
 def setup_config(config_location):
     global config
@@ -47,20 +77,20 @@ def setup_config(config_location):
         config['Q'] = f.read(1)
 
     with open(f'{config_location}Sigma.conf') as f:
-        config['Sigma'] = f.read()
+        config['Sigma'] = f.readline()
 
     with open(f'{config_location}Gamma.conf') as f:
-        config['Gamma'] = f.read()
+        config['Gamma'] = f.readline()
 
     with open(f'{config_location}Delta.conf') as f:
-        config['Delta'] = f.read()
+        config['Delta'] = f.readlines()
 
     with open(f'{config_location}F.conf') as f:
-        config['F'] = f.read()
+        config['F'] = f.readline().split(',')
 
     config['I'] = config['Gamma'][0]
 
-    config['S'] = config['Delta'][0]
+    config['S'] = config['Q']
 
     for x in config:
         print(config[x])
